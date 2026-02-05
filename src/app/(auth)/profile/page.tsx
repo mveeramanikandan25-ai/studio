2de0b18d19d0975cbc55757b8cc765502ae97af0
Veeramanikandan M <mveeramanikandan25@gmail.com>
2026-02-05 09:34:49 +0000

    ## Error Type Runtime ReferenceError
    ## Error Message
    React is not defi

--- src/app/(auth)/profile/page.tsx
+++ src/app/(auth)/profile/page.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Separator } from '@/components/ui/separator';
 import { ProfileClient } from './components/profile-client';
